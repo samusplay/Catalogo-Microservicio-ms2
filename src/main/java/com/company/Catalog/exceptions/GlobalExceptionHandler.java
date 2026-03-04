41637f173ddb2b5c2af6d7743a815ea984c72e22
Kenneth Albadan <albadan.kenneth@gmail.com>
2026-03-04 15:16:55 -0500

    feat:Creación de las excepciones

--- src/main/java/com/company/Catalog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/company/Catalog/exceptions/GlobalExceptionHandler.java
@@ -6,12 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerA
+@RestControllerAdvice
 public class GlobalExceptionHandler extends RuntimeException {
 
     @ExceptionHandler(ProductNotFoundException.class)
